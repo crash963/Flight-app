88018c5b049d30ca2522bc4379cdb2e3b70621dc
crash963 <t-pokorny@seznam.cz>
2021-03-05 13:13:16 +0100

    changed to 5 results

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,6 @@
 import { DateTime } from "luxon";
 
 function Result(props) {
-<<<<<<< HEAD
   const {
     flyFrom,
     flyTo,
@@ -17,7 +16,8 @@ function Result(props) {
   } = props.result;
   return (
     <div className="flight-result">
-      <h4>Flight no.: {flight_no}</h4>
+      {console.log(props)}
+      <h4>Flight no.: {props.result.route[0].flight_no}</h4>
       <p>Price: €{price}</p>
       <p>
         {DateTime.fromSeconds(dTimeUTC).toFormat("dd.MM.y hh:mm")} -{" "}
@@ -28,33 +28,5 @@ function Result(props) {
       <p>To: {cityTo}</p>
     </div>
   );
-=======
-    const {
-        flyFrom,
-        flyTo,
-        dTimeUTC,
-        aTimeUTC,
-        flightNo,
-        cityFrom,
-        cityTo,
-        price,
-        airline,
-        flight_no,
-        fly_duration,
-    } = props.result;
-    return (
-        <div className="flight-result">
-            <h4>Flight no.: {props.result.route[0].flight_no}</h4>
-            <p>Price: €{price}</p>
-            <p>
-                {DateTime.fromSeconds(dTimeUTC).toFormat("dd.MM.y hh:mm")} -{" "}
-                {DateTime.fromSeconds(aTimeUTC).toFormat("dd.MM.y hh:mm")}
-            </p>
-            <p>Flight duration: {fly_duration}</p>
-            <p>From: {cityFrom}</p>
-            <p>To: {cityTo}</p>
-        </div>
-    );
->>>>>>> 4cae2b74c6003e1878dc3b7df676e7ebf39ef97b
 }
 export default Result;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,35 +6,25 @@ function Index() {
   const [result, setResult] = useState(null);
 
   async function fetchData(flightSearch) {
-    console.log(flightSearch);
-    console.log(
-      `https://api.skypicker.com/flights?fly_from=city:${
-        flightSearch.flyFrom
-      }&fly_to=city:${flightSearch.flyTo}&date_from=${
-        flightSearch.dateFrom
-      }&date_to=${flightSearch.dateTo}&partner=picky&sort=date${
-        flightSearch.directFlight ? `&max_stopovers=0` : ""
-      }`
-    );
     const response = await fetch(
       `https://api.skypicker.com/flights?fly_from=city:${
         flightSearch.flyFrom
       }&fly_to=city:${flightSearch.flyTo}&date_from=${
         flightSearch.dateFrom
-      }&date_to=${flightSearch.dateTo}&partner=picky&sort=date${
+      }&date_to=${flightSearch.dateTo}&partner=picky&limit=5&sort=date${
         flightSearch.directFlight ? `&max_stopovers=0` : ""
       }`
     );
     const data = await response.json();
     console.log(data.data);
-    setResult(data.data[0]);
+    setResult(data.data);
   }
 
   return (
     <div>
       <Search fetchData={fetchData} />
       <div className="results">
-        {result ? <Result result={result} /> : null}
+        {result ? result.map((el, key) => <Result result={el} />) : null}
       </div>
     </div>
   );

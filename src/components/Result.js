b8007c0e8aec9eec7cf56ede90ba33853dbfdfa7
Teo15sk <teo15sk@gmail.com>
2021-03-05 12:08:01 +0100

    minor changes to Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,26 +1,30 @@
 import { DateTime } from "luxon";
-
 function Result(props) {
-  const {
-    flyFrom,
-    flyTo,
-    dTimeUTC,
-    aTimeUTC,
-    flightNo,
-    cityFrom,
-    cityTo,
-  } = props.result;
-  return (
-    <div className="flight-result">
-      <h4>Flight no.: {flightNo}</h4>
-      <p>
-        {DateTime.fromSeconds(dTimeUTC).toFormat("dd.MM.y hh:mm")} -{" "}
-        {DateTime.fromSeconds(aTimeUTC).toFormat("dd.MM.y hh:mm")}
-      </p>
-      <p>From: {cityFrom}</p>
-      <p>To: {cityTo}</p>
-    </div>
-  );
+    const {
+        flyFrom,
+        flyTo,
+        dTimeUTC,
+        aTimeUTC,
+        flightNo,
+        cityFrom,
+        cityTo,
+        price,
+        airline,
+        flight_no,
+        fly_duration,
+    } = props.result;
+    return (
+        <div className="flight-result">
+            <h4>Flight no.: {flight_no}</h4>
+            <p>Price: €{price}</p>
+            <p>
+                {DateTime.fromSeconds(dTimeUTC).toFormat("dd.MM.y hh:mm")} -{" "}
+                {DateTime.fromSeconds(aTimeUTC).toFormat("dd.MM.y hh:mm")}
+            </p>
+            <p>Flight duration: {fly_duration}</p>
+            <p>From: {cityFrom}</p>
+            <p>To: {cityTo}</p>
+        </div>
+    );
 }
-
 export default Result;

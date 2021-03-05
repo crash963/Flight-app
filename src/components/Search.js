a750622709efba33b74a4bbd9aee5040db76d97e
Teo15sk <teo15sk@gmail.com>
2021-03-05 15:01:31 +0100

    added default selected city

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,41 +3,43 @@ import { useState } from "react";
 import { DateTime } from "luxon";
 
 function Search(props) {
-  const [flyFrom, setFlyFrom] = useState("");
-  const [flyTo, setFlyTo] = useState("");
-  const [dateFrom, setDateFrom] = useState("");
-  const [dateTo, setDateTo] = useState("");
-  const [directFlight, setDirectFlight] = useState(false);
+    const [flyFrom, setFlyFrom] = useState("VLC");
+    const [flyTo, setFlyTo] = useState("PRG");
+    const [dateFrom, setDateFrom] = useState("");
+    const [dateTo, setDateTo] = useState("");
+    const [directFlight, setDirectFlight] = useState(false);
 
-  return (
-    <form>
-      <label htmlFor="flyFrom">From: </label>
-      <select
-        onChange={(e) => {
-          setFlyFrom(e.target.value);
-        }}
-        name="flyFrom"
-      >
-        <option value="VLC">Valencia</option>
-        <option value="BCN">Barcelona</option>
-        <option value="MAD">Madrid</option>
-        <option value="MIL">Milano</option>
-        <option value="ATH">Athens</option>
-      </select>
+    return (
+        <form>
+            <label htmlFor="flyFrom">From: </label>
+            <select
+                onChange={(e) => {
+                    setFlyFrom(e.target.value);
+                }}
+                name="flyFrom"
+                value={flyFrom}
+            >
+                <option value="VLC">Valencia</option>
+                <option value="BCN">Barcelona</option>
+                <option value="MAD">Madrid</option>
+                <option value="MIL">Milano</option>
+                <option value="ATH">Athens</option>
+            </select>
 
-      <label htmlFor="flyTo">To: </label>
-      <select
-        onChange={(e) => {
-          setFlyTo(e.target.value);
-        }}
-        name="flyTo"
-      >
-        <option value="PRG">Prague</option>
-        <option value="BER">Berlin</option>
-        <option value="WAW">Warsaw</option>
-        <option value="PED">Pardubice</option>
-      </select>
-      {/*  <input
+            <label htmlFor="flyTo">To: </label>
+            <select
+                onChange={(e) => {
+                    setFlyTo(e.target.value);
+                }}
+                name="flyTo"
+                value={flyTo}
+            >
+                <option value="PRG">Prague</option>
+                <option value="BER">Berlin</option>
+                <option value="WAW">Warsaw</option>
+                <option value="PED">Pardubice</option>
+            </select>
+            {/*  <input
         onChange={(e) => {
           setFlyFrom(e.target.value);
         }}
@@ -52,46 +54,50 @@ function Search(props) {
         type="text"
         name="to"
       /> */}
-      <label htmlFor="dateFrom">Date From: </label>
-      <input
-        onChange={(e) => {
-          setDateFrom(DateTime.fromISO(e.target.value).toFormat("dd/MM/yyyy"));
-        }}
-        type="date"
-        name="dateFrom"
-      />
-      <label htmlFor="dateTo">Date To: </label>
-      <input
-        onChange={(e) => {
-          setDateTo(DateTime.fromISO(e.target.value).toFormat("dd/MM/yyyy"));
-        }}
-        type="date"
-        name="dateTo"
-      />
-      <label htmlFor="directFlight">Only Direct Flights</label>
-      <input
-        type="checkbox"
-        name="directFlight"
-        onChange={(e) => {
-          setDirectFlight(e.target.checked);
-        }}
-      />
-      <input
-        type="submit"
-        value="find"
-        onClick={(e) => {
-          e.preventDefault();
-          props.fetchData({
-            flyFrom: flyFrom,
-            flyTo: flyTo,
-            dateFrom: dateFrom,
-            dateTo: dateTo,
-            directFlight: directFlight,
-          });
-        }}
-      />
-    </form>
-  );
+            <label htmlFor="dateFrom">Date From: </label>
+            <input
+                onChange={(e) => {
+                    setDateFrom(
+                        DateTime.fromISO(e.target.value).toFormat("dd/MM/yyyy")
+                    );
+                }}
+                type="date"
+                name="dateFrom"
+            />
+            <label htmlFor="dateTo">Date To: </label>
+            <input
+                onChange={(e) => {
+                    setDateTo(
+                        DateTime.fromISO(e.target.value).toFormat("dd/MM/yyyy")
+                    );
+                }}
+                type="date"
+                name="dateTo"
+            />
+            <label htmlFor="directFlight">Only Direct Flights</label>
+            <input
+                type="checkbox"
+                name="directFlight"
+                onChange={(e) => {
+                    setDirectFlight(e.target.checked);
+                }}
+            />
+            <input
+                type="submit"
+                value="find"
+                onClick={(e) => {
+                    e.preventDefault();
+                    props.fetchData({
+                        flyFrom: flyFrom,
+                        flyTo: flyTo,
+                        dateFrom: dateFrom,
+                        dateTo: dateTo,
+                        directFlight: directFlight,
+                    });
+                }}
+            />
+        </form>
+    );
 }
 
 export default Search;

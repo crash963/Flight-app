612666a71c0bb3d4b43999066ba3622f2f079a66
Teo15sk <teo15sk@gmail.com>
2021-03-05 10:33:50 +0100

    added fetchData function to Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,16 +1,30 @@
+import { useEffect, useState } from "react";
 import Result from "../components/Result";
 import Search from "../components/Search";
 
 function Index() {
-  async function fetchData() {}
-  return (
-    <div>
-      <Search />
-      <div className="results">
-        <Result />
-      </div>
-    </div>
-  );
+    const [result, setResult] = useState({});
+    async function fetchData() {
+        const response = await fetch(
+            "https://api.skypicker.com/flights?fly_from=PRG&fly_to=JFK&date_from=03/04/2021&date_to=15/04/2021"
+        );
+        const data = await response.json();
+        console.log(data);
+        setResult(data);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <Search />
+            <div className="results">
+                <Result result={result} />
+            </div>
+        </div>
+    );
 }
 
 export default Index;

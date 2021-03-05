fc852fd020539cf70d05a591ac0b5d7e7a49543b
Teo15sk <teo15sk@gmail.com>
2021-03-05 10:18:05 +0100

    added Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,5 +1,15 @@
-function Result() {
-  return null;
+function Result(props) {
+    const { flyFrom, flyTo, dateFrom, dateTo, flightNo } = props.result;
+    return (
+        <div className="flight-result">
+            <h4>Flight no.: {flightNo}</h4>
+            <p>
+                {dateFrom} - {dateTo}
+            </p>
+            <p>From:{flyFrom}</p>
+            <p>To: {flyTo}</p>
+        </div>
+    );
 }
 
 export default Result;

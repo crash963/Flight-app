9a55c15150033573cce5312bc52a452379c8e9ae
Teo15sk <teo15sk@gmail.com>
2021-03-05 13:53:31 +0100

    minor changes to Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,33 +1,32 @@
 import { DateTime } from "luxon";
 
 function Result(props) {
-  const {
-    flyFrom,
-    flyTo,
-    dTimeUTC,
-    aTimeUTC,
-    flightNo,
-    cityFrom,
-    cityTo,
-    price,
-    airline,
-    flight_no,
-    fly_duration,
-  } = props.result;
-  return (
-    <div className="flight-result">
-      {console.log(props)}
-      <h4>Flight no.: {props.result.route[0].flight_no}</h4>
-      <p>Price: €{price}</p>
-      <p>
-        {DateTime.fromSeconds(dTimeUTC).toFormat("dd.MM.y hh:mm")} -{" "}
-        {DateTime.fromSeconds(aTimeUTC).toFormat("dd.MM.y hh:mm")}
-      </p>
-      <p>Flight duration: {fly_duration}</p>
-      <p>From: {cityFrom}</p>
-      <p>To: {cityTo}</p>
-      <p>Number of stopovers: {props.result.route.length -1}</p>
-    </div>
-  );
+    const {
+        dTimeUTC,
+        aTimeUTC,
+        cityFrom,
+        cityTo,
+        price,
+        fly_duration,
+        route,
+    } = props.result;
+    return (
+        <div className="flight-result">
+            {console.log(props)}
+            <h4>
+                Flight no.: {route[0].airline}
+                {route[0].flight_no}
+            </h4>
+            <p>Price: €{price}</p>
+            <p>
+                {DateTime.fromSeconds(dTimeUTC).toFormat("dd.MM.y hh:mm")} -{" "}
+                {DateTime.fromSeconds(aTimeUTC).toFormat("dd.MM.y hh:mm")}
+            </p>
+            <p>Flight duration: {fly_duration}</p>
+            <p>From: {cityFrom}</p>
+            <p>To: {cityTo}</p>
+            <p>Number of stopovers: {route.length - 1}</p>
+        </div>
+    );
 }
 export default Result;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -3,31 +3,33 @@ import Result from "../components/Result";
 import Search from "../components/Search";
 
 function Index() {
-  const [result, setResult] = useState(null);
+    const [result, setResult] = useState(null);
 
-  async function fetchData(flightSearch) {
-    const response = await fetch(
-      `https://api.skypicker.com/flights?fly_from=city:${
-        flightSearch.flyFrom
-      }&fly_to=city:${flightSearch.flyTo}&date_from=${
-        flightSearch.dateFrom
-      }&date_to=${flightSearch.dateTo}&partner=picky&limit=5&sort=date${
-        flightSearch.directFlight ? `&max_stopovers=0` : ""
-      }`
+    async function fetchData(flightSearch) {
+        const response = await fetch(
+            `https://api.skypicker.com/flights?fly_from=city:${
+                flightSearch.flyFrom
+            }&fly_to=city:${flightSearch.flyTo}&date_from=${
+                flightSearch.dateFrom
+            }&date_to=${flightSearch.dateTo}&partner=picky&limit=5&sort=date${
+                flightSearch.directFlight ? `&max_stopovers=0` : ""
+            }`
+        );
+        const data = await response.json();
+        console.log(data.data);
+        setResult(data.data);
+    }
+
+    return (
+        <div>
+            <Search fetchData={fetchData} />
+            <div className="results">
+                {result
+                    ? result.map((el, key) => <Result result={el} />)
+                    : null}
+            </div>
+        </div>
     );
-    const data = await response.json();
-    console.log(data.data);
-    setResult(data.data);
-  }
-
-  return (
-    <div>
-      <Search fetchData={fetchData} />
-      <div className="results">
-        {result ? result.map((el, key) => <Result result={el} />) : null}
-      </div>
-    </div>
-  );
 }
 
 export default Index;

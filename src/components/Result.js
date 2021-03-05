fb9df8f2a7c7c56bc9a6ed3eff21c36af9630a78
crash963 <t-pokorny@seznam.cz>
2021-03-05 11:32:53 +0100

    finished fetchData

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,26 +1,26 @@
 import { DateTime } from "luxon";
 
 function Result(props) {
-    const {
-        flyFrom,
-        flyTo,
-        dTime,
-        aTime,
-        flightNo,
-        cityFrom,
-        cityTo,
-    } = props.result;
-    return (
-        <div className="flight-result">
-            <h4>Flight no.: {flightNo}</h4>
-            <p>
-                {DateTime.fromSeconds(dTime).toFormat("dd.MM.y hh:mm")} -{" "}
-                {DateTime.fromSeconds(aTime).toFormat("dd.MM.y hh:mm")}
-            </p>
-            <p>From: {cityFrom}</p>
-            <p>To: {cityTo}</p>
-        </div>
-    );
+  const {
+    flyFrom,
+    flyTo,
+    dTimeUTC,
+    aTimeUTC,
+    flightNo,
+    cityFrom,
+    cityTo,
+  } = props.result;
+  return (
+    <div className="flight-result">
+      <h4>Flight no.: {flightNo}</h4>
+      <p>
+        {DateTime.fromSeconds(dTimeUTC).toFormat("dd.MM.y hh:mm")} -{" "}
+        {DateTime.fromSeconds(aTimeUTC).toFormat("dd.MM.y hh:mm")}
+      </p>
+      <p>From: {cityFrom}</p>
+      <p>To: {cityTo}</p>
+    </div>
+  );
 }
 
 export default Result;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -3,15 +3,15 @@ import Result from "../components/Result";
 import Search from "../components/Search";
 
 function Index() {
-  const [result, setResult] = useState({});
+  const [result, setResult] = useState(null);
 
   async function fetchData(flightSearch) {
     console.log(flightSearch);
     const response = await fetch(
-      `https://api.skypicker.com/flights?fly_from=${flightSearch.flyFrom}&fly_to=${flightSearch.flyTo}&date_from=${flightSearch.dateFrom}&date_to=${flightSearch.dateTo}&partner=picky`
+      `https://api.skypicker.com/flights?fly_from=${flightSearch.flyFrom}&fly_to=${flightSearch.flyTo}&date_from=${flightSearch.dateFrom}&date_to=${flightSearch.dateTo}&partner=picky&sort=date`
     );
     const data = await response.json();
-
+    console.log(data.data);
     setResult(data.data[0]);
   }
 
@@ -19,7 +19,7 @@ function Index() {
     <div>
       <Search fetchData={fetchData} />
       <div className="results">
-        <Result result={result} />
+        {result ? <Result result={result} /> : null}
       </div>
     </div>
   );

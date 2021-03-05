9ae8b4b0ed2d6ccc5a0acedfc8d791bb6f76e250
Tomas Nejedly <t.nejedly1@gmail.com>
2021-03-05 13:15:35 +0100

    merging

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,34 +1,6 @@
 import { DateTime } from "luxon";
 
 function Result(props) {
-<<<<<<< HEAD
-  const {
-    flyFrom,
-    flyTo,
-    dTimeUTC,
-    aTimeUTC,
-    flightNo,
-    cityFrom,
-    cityTo,
-    price,
-    airline,
-    flight_no,
-    fly_duration,
-  } = props.result;
-  return (
-    <div className="flight-result">
-      <h4>Flight no.: {flight_no}</h4>
-      <p>Price: €{price}</p>
-      <p>
-        {DateTime.fromSeconds(dTimeUTC).toFormat("dd.MM.y hh:mm")} -{" "}
-        {DateTime.fromSeconds(aTimeUTC).toFormat("dd.MM.y hh:mm")}
-      </p>
-      <p>Flight duration: {fly_duration}</p>
-      <p>From: {cityFrom}</p>
-      <p>To: {cityTo}</p>
-    </div>
-  );
-=======
     const {
         flyFrom,
         flyTo,
@@ -52,9 +24,9 @@ function Result(props) {
             </p>
             <p>Flight duration: {fly_duration}</p>
             <p>From: {cityFrom}</p>
-            <p>To: {cityTo}</p>
+            <p>To: {cityTo}</p>   
+            
         </div>
     );
->>>>>>> 4cae2b74c6003e1878dc3b7df676e7ebf39ef97b
 }
 export default Result;
